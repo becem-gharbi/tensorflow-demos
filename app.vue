e5657a6871c7abed04b46e8cb34827e04ee5b56d
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-02-27 18:55:06 +0100

    refactor: add UI modules

--- app.vue
+++ app.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NaiveConfig :theme-config="themeConfig">
+    <NaiveNavbar :routes="routes" menu-inverted>
+
+      <template #start>
+        <NuxtLink to="/">
+          <div class=" flex items-center gap-3">
+            <NaiveIcon name="logos:tensorflow" :size="30"></NaiveIcon>
+            <n-h4 class="m-0">TensorFlow demos</n-h4>
+          </div>
+        </NuxtLink>
+      </template>
+
+      <template #end>
+        <NuxtLink to="https://github.com/becem-gharbi/tensorflow-demos" target="_blank">
+          <n-button text>
+            <NaiveIcon name="codicon:github"></NaiveIcon>
+          </n-button>
+        </NuxtLink>
+        <NaiveColorModeSwitch></NaiveColorModeSwitch>
+      </template>
+    </NaiveNavbar>
+    <NuxtPage />
+  </NaiveConfig>
 </template>
+
+<script setup lang="ts">
+import { NavbarRoute, ThemeConfig } from "@bg-dev/nuxt-naiveui"
+
+const routes: NavbarRoute[] = [
+  {
+    label: "Pre-trained",
+    icon: "ic:baseline-check",
+    path: "/object-detection"
+  },
+  {
+    label: "Re-trained",
+    icon: "material-symbols:autorenew"
+  }
+]
+
+const themeConfig: ThemeConfig = {
+  shared: {
+    common: {
+      fontFamily: "Gellix"
+    }
+  }
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  css: ["~/assets/fonts/gellix/style.css"],
+  modules: ["@bg-dev/nuxt-naiveui", "@nuxtjs/tailwindcss"],
+});
